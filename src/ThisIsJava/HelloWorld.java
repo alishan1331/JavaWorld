35dfd9228319bedfabfeea22736c1802cd8698cb
alishan <alishan@10.0.0.226>
2024-01-21 17:03:21 -0500

    fixed S

--- src/ThisIsJava/HelloWorld.java
+++ src/ThisIsJava/HelloWorld.java
@@ -4,16 +4,16 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Monday");
-		
+
 		System.out.println("Sprint 1 work");
 	}
-	
+
 	public static void addSum() {
 		System.out.println("Sending to git");
 	}
-public void goBack(){
-system.out.println("go back to eclipse");
-	
-}
-	}
 
+	public void goBack() {
+		System.out.println("go back to eclipse");
+
+	}
+}
