5b3652624f5204719d6e74a26b9c1d2b38902531
alishan <alishan@10.0.0.226>
2024-01-21 16:43:38 -0500

    initial commit

---
+++ src/ThisIsJava/HelloWorld.java
@@ -0,0 +1,12 @@
+package ThisIsJava;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Monday");
+		
+		System.out.println("Sprint 1 work");
+	}
+	
+	
+}
